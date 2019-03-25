972a93eeecfe49277c6baa30644ed568520bb0b4
Lucas Siqueira <lucassiqzro@gmail.com>
2019-03-25 11:38:17 -0300

    Adicionando busca sequencial com sentinela

--- algoritimos.py
+++ algoritimos.py
@@ -10,6 +10,15 @@ def busca_sequencial(lista, alvo):
             return i
     return -1
 
+def busca_sequencial_com_sentinela(lista, alvo):
+    i = 0
+    lista.append(alvo)
+    while lista[i] != alvo:
+        i += 1
+    if i == (len(lista) -1):
+        return -1
+    return i
+
 def busca_binaria(lista, alvo):
     primeiro = 0
     ultimo = len(lista) - 1
@@ -31,10 +40,11 @@ def busca_binaria(lista, alvo):
     return -1
 
 # pip3 install matplotlib
-def plota_grafico(sequencial, binaria):
+def plota_grafico(sequencial, binaria, sequencial_com_sentinela):
     plt.ylabel('time')
     plt.bar("binaria", binaria)
     plt.bar("sequencial", sequencial)
+    plt.bar("sequencial com sentinela",sequencial_com_sentinela)
     plt.show()
 
 lista = [x for x in range(1, 1001)]
@@ -53,8 +63,15 @@ print("\n", busca_sequencial(lista, 750))
 fim = time()
 
 sequencial = Decimal(fim - inicio)
+print(Decimal(fim - inicio))
 
+
+inicio = time()
+print("\n", busca_sequencial_com_sentinela(lista, 750))
+fim = time()
+
+sequencial_com_sentinela = Decimal(fim - inicio)
 
 print(Decimal(fim - inicio))
 
-plota_grafico(sequencial, binaria)
+plota_grafico(sequencial, binaria, sequencial_com_sentinela)
