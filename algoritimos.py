fed965e24ea2079cfda36dfde28c5edadf6d9d65
Lucas Siqueira <lucassiqzro@gmail.com>
2019-03-26 16:10:53 -0300

    Criando indicador de número de vezes que as buscas são mais efetivas que outras

--- algoritimos.py
+++ algoritimos.py
@@ -1,5 +1,6 @@
 from time import time
 from decimal import Decimal
+from random import randint
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -30,7 +31,7 @@ def busca_binaria(lista, alvo):
     while primeiro <= ultimo:
         meio = (primeiro + ultimo) // 2
 
-        print(i, meio)
+        #print(i, meio)
         i += 1
 
         if lista[meio] == alvo:
@@ -44,37 +45,62 @@ def busca_binaria(lista, alvo):
 
 # pip3 install matplotlib
 # pip3 install python3-tk
-def plota_grafico(sequencial, binaria, sequencial_com_sentinela):
+def plota_grafico(binaria, sequencial_com_sentinela, sequencial):
     plt.ylabel('time')
     plt.bar("binaria", binaria)
-    plt.bar("sequencial", sequencial)
     plt.bar("sequencial com sentinela",sequencial_com_sentinela)
+    plt.bar("sequencial", sequencial)
     plt.show()
 
-lista = [x for x in range(1, 1001)]
 
-# print(lista)
+wins_binaria = 0
+wins_sequencial = 0
+wins_sequencial_sentinela = 0
+aux = 0
 
-inicio = time()
-print("\n", busca_binaria(lista, 750))
-fim = time()
+while aux != 1000:
+    lista = [x for x in range(randint(1,1001))]
 
-binaria = Decimal(fim - inicio)
-print(Decimal(fim - inicio))
+    # print(len(lista))
 
-inicio = time()
-print("\n", busca_sequencial(lista, 750))
-fim = time()
+    numero_aleatoriio = randint(1,len(lista))
 
-sequencial = Decimal(fim - inicio)
-print(Decimal(fim - inicio))
+    inicio = time()
 
-inicio = time()
-print("\n", busca_sequencial_com_sentinela(lista, 750))
-fim = time()
+    print("\n", busca_binaria(lista, numero_aleatoriio))
+    # busca_binaria(lista, numero_aleatoriio)
+    fim = time()
 
-sequencial_com_sentinela = Decimal(fim - inicio)
+    binaria = Decimal(fim - inicio)
+    print(Decimal(fim - inicio))
 
-print(Decimal(fim - inicio))
+    inicio = time()
+    print("\n", busca_sequencial(lista, numero_aleatoriio))
+    # busca_sequencial(lista, numero_aleatoriio)
+    fim = time()
 
-plota_grafico(sequencial, binaria, sequencial_com_sentinela)
+    sequencial = Decimal(fim - inicio)
+    print(Decimal(fim - inicio))
+
+    inicio = time()
+    print("\n", busca_sequencial_com_sentinela(lista, numero_aleatoriio))
+    # busca_sequencial_com_sentinela(lista, numero_aleatoriio)
+    fim = time()
+
+    sequencial_com_sentinela = Decimal(fim - inicio)
+
+    print(Decimal(fim - inicio))
+
+    if sequencial < binaria and sequencial < sequencial_com_sentinela:
+        wins_sequencial += 1
+    elif binaria < sequencial and binaria < sequencial_com_sentinela:
+        wins_binaria += 1
+    elif sequencial_com_sentinela < binaria and sequencial_com_sentinela < sequencial:
+        wins_sequencial_sentinela += 1
+
+    aux += 1
+
+print("\n\t \t \tResultado em Wins: \n Sequencial:" + str(wins_sequencial) + '\n',"Binaria:" + str(wins_binaria) + '\n',"Sequencial com sentinela:" + str(wins_sequencial_sentinela) + '\n')
+print("Empate entre pelo menos dois algoritmos de busca: " + str(aux - (wins_sequencial + wins_binaria + wins_sequencial_sentinela)))
+
+plota_grafico(wins_binaria, wins_sequencial_sentinela, wins_sequencial)
