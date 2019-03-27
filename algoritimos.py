3f2209bde94e3294fa12801defadc2a3b764ac84
Lucas Siqueira <lucassiqzro@gmail.com>
2019-03-26 21:28:15 -0300

    Pequenas correções

--- algoritimos.py
+++ algoritimos.py
@@ -68,19 +68,21 @@ wins_sequencial_sentinela = 0
 aux = 0
 
 while aux != 1000:
+    # Define uma lista já ordenada com tamanho aléatorio no intervalo (1,1001)
     lista = [x for x in range(randint(1,1001))]
 
-    numero_aleatoriio = randint(1,len(lista))
+    #Define um número aleatorio da lista a ser buscado
+    numero_aleatorio = randint(1,len(lista))
 
     inicio = time()
-    busca_binaria(lista, numero_aleatoriio)
+    busca_binaria(lista, numero_aleatorio)
     fim = time()
 
     binaria = Decimal(fim - inicio)
     resultado_binaria.append(binaria)
 
     inicio = time()
-    busca_sequencial(lista, numero_aleatoriio)
+    busca_sequencial(lista, numero_aleatorio)
     fim = time()
 
     sequencial = Decimal(fim - inicio)
@@ -105,7 +107,7 @@ while aux != 1000:
 print("\n\t \t \tResultado em Wins: \n Binaria:" + str(wins_binaria) + '\n' ,"Sequencial:" + str(wins_sequencial) + '\n',"Sequencial com sentinela:" + str(wins_sequencial_sentinela) + '\n')
 print("Empate entre pelo menos dois algoritmos de busca: " + str(aux - (wins_sequencial + wins_binaria + wins_sequencial_sentinela)))
 print("\n\t\tMédia de tempo\n")
-print("Binaria:" + str(define_media(resultado_binaria)), "Sequencial:" + str(define_media(resultado_sequencial)),"Sequencial com sentinela:" + str(define_media(resultado_sequencial_sentinela)))
+print("Binaria:" + str(define_media(resultado_binaria)), "\nSequencial:" + str(define_media(resultado_sequencial)),"\nSequencial com sentinela:" + str(define_media(resultado_sequencial_sentinela)))
 
 plota_grafico(wins_binaria, wins_sequencial_sentinela, wins_sequencial, 'Wins')
 plota_grafico(define_media(resultado_binaria), define_media(resultado_sequencial_sentinela), define_media(resultado_sequencial), 'Média de tempo')
